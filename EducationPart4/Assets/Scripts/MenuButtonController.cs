b087a00ffca9962d62126df45a9b26536bdee060
Evg Pn <ye.ponomarenko@gmail.com>
2020-04-12 17:35:07 +0300

    feat(project) add side menu
    main screen has button of main menu wich slides on left side

---
+++ EducationPart4/Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour
+{
+    [SerializeField] private GameObject _hideMenuButton = null;
+    [SerializeField] private Animator _menuButtonAnimator = null;
+    private bool _menuSliding = false;
+
+    private void Awake()
+    {
+        _hideMenuButton.SetActive(false);
+    }
+
+    public void ShowMenu()
+    {
+        if(!_menuSliding)
+        {
+            ChangeMenuState(!_menuSliding);
+        }
+    }
+
+    public void HideMenu()
+    {
+        if(_menuSliding)
+        {
+            ChangeMenuState(!_menuSliding);
+        }
+    }
+
+    private void ChangeMenuState(bool menuSliding)
+    {
+        _menuSliding = menuSliding;
+        _menuButtonAnimator.SetBool("Sliding", _menuSliding);
+        _hideMenuButton.SetActive(_menuSliding);
+    }
+}
